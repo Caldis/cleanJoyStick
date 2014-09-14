63e27e2853f193c753e04d6f046879542cb8303f
Caldis <532607446@qq.com>
2014-09-14 16:26:15 +0800

    Initial Commit

---
+++ cleanJoyStickTests/cleanJoyStickTests.m
@@ -0,0 +1,34 @@
+//
+//  cleanJoyStickTests.m
+//  cleanJoyStickTests
+//
+//  Created by Cyrus on 14-9-14.
+//  Copyright (c) 2014年 Cb. All rights reserved.
+//
+
+#import <XCTest/XCTest.h>
+
+@interface cleanJoyStickTests : XCTestCase
+
+@end
+
+@implementation cleanJoyStickTests
+
+- (void)setUp
+{
+    [super setUp];
+    // Put setup code here. This method is called before the invocation of each test method in the class.
+}
+
+- (void)tearDown
+{
+    // Put teardown code here. This method is called after the invocation of each test method in the class.
+    [super tearDown];
+}
+
+- (void)testExample
+{
+    XCTFail(@"No implementation for \"%s\"", __PRETTY_FUNCTION__);
+}
+
+@end
